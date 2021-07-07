26c3a6f1630dca8aed3134631ce17d3860fb7712
Luis Ruiz <66579647+LRE323@users.noreply.github.com>
2021-07-07 10:55:10 -0700

    Added Home and Favorites tabs, bottom navigation

--- app/src/main/java/com/luisruiz/moviebuddy/ui/dashboard/FavoritesFragment.kt
+++ app/src/main/java/com/luisruiz/moviebuddy/ui/dashboard/FavoritesFragment.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.luisruiz.moviebuddy.R
 import com.luisruiz.moviebuddy.databinding.FragmentFavoritesBinding
 
-class DashboardFragment : Fragment() {
+class FavoritesFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var favoritesViewModel: FavoritesViewModel
     private var _binding: FragmentFavoritesBinding? = null
 
     // This property is only valid between onCreateView and
@@ -25,8 +22,8 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        favoritesViewModel =
+            ViewModelProvider(this).get(FavoritesViewModel::class.java)
 
         _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         val root: View = binding.root
